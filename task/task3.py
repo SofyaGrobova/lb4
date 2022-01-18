0288b4600148f8a6dace5d5248a399876d221487
SofyaGrobova <sofya.grobova@yandex.ru>
2022-01-18 13:23:00 +0300

    g

---
+++ task/task3.py
@@ -0,0 +1,7 @@
+import math
+
+osn_one = int(input('Osnovanie 1:'))
+osn_two = int(input('Osnovanie 2: '))
+hight = int(input('Hight: '))
+
+print('Ploshad: ' + str((osn_one + osn_two) * hight / 2))
